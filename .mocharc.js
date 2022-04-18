ddb09a90350f6ae737b8019c69c707ceed6721b9
Saurabh Prakash <saurabhp@lambdatest.com>
2022-04-18 18:49:37 +0530

    Added .tas.yml

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,12 @@
 module.exports = {
+  require: [
+    "node_modules/reflect-metadata/Reflect.js",
+    "ts-node/register"
+  ],
   mochaHooks: {
     beforeAll: function() {
       require('reflect-metadata');
     }
-  }
+  },
+  exit: true
 };
